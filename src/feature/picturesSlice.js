8e7bdde6b64819c4ef26b0377df63bfe29b4c7fb
kazi <champlonremii@gmail.com>
2022-09-04 11:00:42 +0200

    edit and delete

--- src/feature/picturesSlice.js
+++ src/feature/picturesSlice.js
@@ -12,10 +12,27 @@ export const picturesSlice = createSlice({
         },
         addPicture: (state, action) => {
             state.pictures.push(action.payload)
+        },
+        editPicture: (state, action) => {
+          state.pictures = state.pictures.map((pic => {
+              if (pic.id === action.payload[1]) {
+                  return {
+                      ...pic,
+                      artist: action.payload[0],
+                  };
+              } else {
+                  return pic;
+              }
+          }))
+        },
+        deletePicture: (state, action) => {
+            state.pictures = state.pictures.filter((pic) => pic.id !== action.payload)
         }
     }
 });
 
 export const {setPicturesData} = picturesSlice.actions;
 export const {addPicture} = picturesSlice.actions;
+export const {editPicture} = picturesSlice.actions;
+export const {deletePicture} = picturesSlice.actions;
 export default picturesSlice.reducer;
