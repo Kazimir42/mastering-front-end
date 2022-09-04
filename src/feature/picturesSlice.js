9d5bc30e5a577854cb94528743899499b70073a1
kazi <champlonremii@gmail.com>
2022-09-04 10:35:07 +0200

    post

--- src/feature/picturesSlice.js
+++ src/feature/picturesSlice.js
@@ -9,9 +9,13 @@ export const picturesSlice = createSlice({
     reducers: {
         setPicturesData: (state, action) => {
             state.pictures = action.payload;
+        },
+        addPicture: (state, action) => {
+            state.pictures.push(action.payload)
         }
     }
 });
 
 export const {setPicturesData} = picturesSlice.actions;
+export const {addPicture} = picturesSlice.actions;
 export default picturesSlice.reducer;
